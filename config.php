6d9befa20afec31c6b434ed7125e7d016fcc233b
Naveen <p6naveen@gmail.com>
2016-06-14 17:07:36 +0530

    redirect url changed.

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
  */
 define("CONSUMER_KEY", "3MVG9ZL0ppGP5UrAYBQz1g4J.6V3CyaV9a9fG5lriCvGZ.npGUkpIE22OciPlevXuAAwqyJeZQVEuk9F4LR9T");
 
-define("REDIRECT_URL", "https://staging.buzzboard.com/force_versions/naveen/callback.php");
+define("REDIRECT_URL", "https://example.com/callback.php");
 
 define("SALESFORCE_URL", "https://login.salesforce.com/services/oauth2");
 
